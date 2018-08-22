e9c65c13b0a4ed8b8b88e7dc2bdb336996ac94ac
Annie Eby <eby.annie@gmail.com>
2018-08-22 22:24:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,3 @@ function updateObjectWithKeyAndValue(object, key, value){
     object[key] = value
     return object
 }
-
-function updateObjectWithKeyAndValue(object, key, value){
-  var copy = Object.assign({}, object);
-console.log(copy)
-}
