818687c73d2266905a5f7ebf2c353ab280fd4e7e
Annie Eby <eby.annie@gmail.com>
2018-08-22 23:40:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 function deleteFromObjectByKey(object, key){
   var newobject = Object.assign({},object,{})
   delete newobject[key]
+  return newobject
 }
 
 
