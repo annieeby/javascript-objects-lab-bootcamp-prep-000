89311ef861e49a32a33d9cc6b1727dde45c1c68f
Annie Eby <eby.annie@gmail.com>
2018-08-22 22:14:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value){
+  object, artistName, songTitle) {
+    playlist[artistName] = songTitle
+    return playlist
+}
+
   return
 }
