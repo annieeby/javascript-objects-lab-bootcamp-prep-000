cb9a885c3f1f26bcb4f2e368f8e882ad8ae1c34c
Annie Eby <eby.annie@gmail.com>
2018-08-22 22:32:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,3 @@ var recipes = {};
 function updateObjectWithKeyAndValue(object, key, value){
     return Object.assign({},object,{[key]:value})
 }
-
-
-/*
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  var copy = Object.assign({}, object);
-console.log(copy)
-}
